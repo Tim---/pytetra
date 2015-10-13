1244771fbffa082f2aec36e62e2dc0a8eca3be9c
Timothée COCAULT <timothee.cocault@gmail.com>
2015-10-13 06:49:05 +0200

    Remove test code

--- pytetra/layer/mac/defragmenter.py
+++ pytetra/layer/mac/defragmenter.py
@@ -9,16 +9,12 @@ class MacDefragmenter(object):
     def process_pdu(self, pdu):
         if isinstance(pdu, MacResourcePdu):
             if pdu.length_indication == 63:
-                print "DEBUG", "Start"
                 self.fragments = [pdu]
             else:
-                print "DEBUG", "Normal"
                 return pdu
         elif isinstance(pdu, MacFrag):
-            print "DEBUG", "Frag"
             self.fragments.append(pdu)
         elif isinstance(pdu, MacEnd):
-            print "DEBUG", "End"
             self.fragments.append(pdu)
             sdu = Bits(''.join(f.sdu.bits for f in self.fragments))
             pdu = self.fragments[0]
